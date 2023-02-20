baf42766739638cada47c9c5263b2c6f24e17d58
Nadia Magazine <77028660+nadiamagazine@users.noreply.github.com>
2023-02-20 16:12:08 -0500

    add image

--- app/src/main/java/com/example/harvardartmuseumsproject/GalleryListDetailsScreen.kt
+++ app/src/main/java/com/example/harvardartmuseumsproject/GalleryListDetailsScreen.kt
@@ -5,17 +5,26 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.harvardartmuseumsproject.model.Object
 import com.example.harvardartmuseumsproject.viewmodel.GalleryListDetailsViewModel
 
@@ -65,24 +74,55 @@ fun GroupRow(
             shape = RoundedCornerShape(8.dp),
             elevation = 4.dp
         ) {
-            Column(
+            Row(
                 Modifier
                     .padding(4.dp)
                     .fillMaxSize()
             ) {
-                obj.objectNumber.let {
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.body1,
-                        fontWeight = FontWeight.Bold
+                val imageUrl = obj.primaryImageUrl
+                if (imageUrl != null) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(imageUrl)
+                            .crossfade(true)
+                            .build(),
+                        // placeholder = painterResource(R.mipmap.ic_launcher),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .size(100.dp)
+                            .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
                     )
+                } else {
+                    Text(text = "No image available)")
+//                    Image(
+//                        painter = painterResource(R.mipmap.ic_launcher),
+//                        contentDescription = null,
+//                        modifier = Modifier
+//                            .padding(8.dp)
+//                            .size(85.dp)
+//                            .clip(RoundedCornerShape(corner = CornerSize(16.dp))))
                 }
-                obj.description?.let {
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.body1,
-                        fontWeight = FontWeight.Bold
-                    )
+                Column(
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .fillMaxSize()
+                        .align(Alignment.CenterVertically)
+                ) {
+                    obj.objectNumber.let {
+                        Text(
+                            text = it,
+                            style = MaterialTheme.typography.body1,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    obj.description?.let {
+                        Text(
+                            text = it,
+                            style = MaterialTheme.typography.body1,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
             }
         }
