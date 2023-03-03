8d0b66e3c70946ccfaef88998fba82641d2466fe
Nadia Magazine <77028660+nadiamagazine@users.noreply.github.com>
2023-03-03 09:48:54 -0500

    check if the same picture api calls

--- app/src/main/java/com/example/harvardartmuseumsproject/FullSizeImageScreen.kt
+++ app/src/main/java/com/example/harvardartmuseumsproject/FullSizeImageScreen.kt
@@ -1,11 +1,11 @@
 package com.example.harvardartmuseumsproject
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
@@ -17,7 +17,9 @@ import com.example.harvardartmuseumsproject.viewmodel.FullSizeImageViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.harvardartmuseumsproject.model.ArtObject
 import com.example.harvardartmuseumsproject.model.FullSizeImage
+import com.example.harvardartmuseumsproject.viewmodel.GalleryListDetailsViewModel
 
 
 @Composable
@@ -36,6 +38,7 @@ fun FullSizeImageScreen(
         viewState.value?.let {
            ImageScreen(
                 fullSizeImage = it,
+               secondImage = it,
                 navController = navController
             )
         } ?: ErrorHandlingMessage()
@@ -45,35 +48,67 @@ fun FullSizeImageScreen(
 @Composable
 fun ImageScreen(
     fullSizeImage: FullSizeImage,
+    secondImage: FullSizeImage,
     navController: NavController
 ) {
-    val imageUrl = fullSizeImage.primaryImageUrl
-    if (imageUrl != null) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(imageUrl)
-                .crossfade(true)
-                .build(),
-            contentDescription = null,
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxSize()
-                .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
-        )
+    Column(
+        Modifier
+            .padding(4.dp)
+            .fillMaxSize()
+    ) {
+        val imageUrl = fullSizeImage.primaryImageUrl
+        if (imageUrl != null) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .build(),
+                // placeholder = painterResource(R.mipmap.ic_launcher),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(300.dp)
+                    .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+            )
+        } else {
+            Text(
+                text = "No image available)",
+                modifier = Modifier
+                    //    .align(Alignment.CenterStart)
+                    .padding(8.dp)
+                    .size(300.dp)
+                    .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+            )
+        }
+        val image12 = fullSizeImage.primaryImageUrl
+        if (image12 != null) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .build(),
+                // placeholder = painterResource(R.mipmap.ic_launcher),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(300.dp)
+                    .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+            )
+        }
     }
 }
 
-//@Composable
-//fun GroupList(
-//    navController: NavController,
-//    listOfObjects: List<FullSizeImage>
-//) {
-//    LazyColumn {
-//        itemsIndexed(items = listOfObjects) { index, item ->
-//           ImageScreen(
-//                navController = navController,
-//                fullSizeImage = item
-//            )
+//    @Composable
+//    fun ImageList(
+//        navController: NavController,
+//        listOfObjects: List<FullSizeImage>
+//    ) {
+//        LazyColumn {
+//            itemsIndexed(items = listOfObjects) { index, item ->
+//                ImageScreen(
+//                    navController = navController,
+//                    fullSizeImage = item
+//                )
+//            }
 //        }
 //    }
-//}
